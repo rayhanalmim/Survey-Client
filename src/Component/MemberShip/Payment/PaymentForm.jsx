d70212b4a5b8213dfbf958966e3acf51ea71cb46
rayhanalmim <rayhanalmim1@gmail.com>
2023-11-26 16:58:55 +0600

    payment method integrate successfully

---
+++ src/Component/MemberShip/Payment/PaymentForm.jsx
@@ -0,0 +1,122 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import './style.css'
+import { useContext, useState } from "react";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+import { AuthContext } from "../../../Authentication/AuthProvider";
+import useAxiosSecure from "../../../Hook/useAxiosSecure";
+
+const PaymentForm = () => {
+    const {user} = useContext(AuthContext);
+    const stripe = useStripe();
+    const elements = useElements();
+    const [clientSecret, setClientSecret] = useState("");
+    const axiosSecure = useAxiosSecure()
+
+    const totalPrice = 100;
+
+    useEffect(()=>{
+        axiosSecure.post('/create-payment-intent',{price: totalPrice})
+        .then(res=>{
+            setClientSecret(res.data.clientSecret)
+        })
+    }, [axiosSecure, totalPrice])
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+            return;
+        }
+
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        })
+        if(error){
+            console.log('payment error:',error)
+            Swal.fire({
+                title: "Oops...",
+                text: error?.message,
+                icon: "error"
+              });
+        }
+        else{
+            console.log('payment method:',paymentMethod)
+        }
+
+        console.log(clientSecret)
+
+        // ---------------------confirmPayment----------------------
+        const {paymentIntent, error: paymentConfirmError} = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email || 'anonymous',
+                    name: user?.displayName || 'anonymous',
+                }
+            }
+        })
+        if(error){
+            console.log(paymentConfirmError);
+        }
+        else{
+            console.log('payment success:',paymentIntent)
+            if(paymentIntent.status === 'succeeded'){
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `Payment successfull`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+
+                const paymentInfo = {
+                    email: user.email,
+                    date: new Date(),
+                    paymentStatus : 'success',
+                    totalAmount: totalPrice,
+                    PaymentId: paymentIntent.payment_method,
+                }
+                console.log(paymentInfo);
+                const saveDatabase = await axiosSecure.post('/payment', paymentInfo)
+                console.log(saveDatabase.data)
+            }
+        }
+    }
+
+    return (
+        <div className="">
+            <form className="lg:pl-2" onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <button className="btn text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 text-center " type="submit" disabled={!stripe || !clientSecret}>
+                Comfirm Payment
+            </button>
+            
+        </form>
+        </div>
+    );
+};
+
+export default PaymentForm;
--- src/Component/MemberShip/UpgradePro.jsx
+++ src/Component/MemberShip/UpgradePro.jsx
@@ -1,7 +1,26 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import PaymentForm from "./Payment/PaymentForm";
+
+const stripePromise = loadStripe('pk_test_51OEnfwEKCviqmDKyovTyS1fPNeXHWySowVMQJgXcRuVjxifQIoOTvKA3eYpVy0DM7WZryJocappXPKsZnzJd92ej00HUuuu5Ew');
+
 const UpgradePro = () => {
     return (
-        <div>
-            <h3>upgrade feature comming soon</h3>
+        <div className="px-7 bg-[#F5FF90]">
+            <div className="bg-[url('https://i.ibb.co/23kSRq3/membership-model.jpg')] bg-cover bg-center h-3/4 md:h-full w-full rounded mb-4">
+                <div className="text-center inset-0 bg-black bg-opacity-60 bg-blend-multiply py-28 space-y-3">
+                    <h3 className="text-white text-3xl font-bold">Unlock Premium Features with Our Exclusive Membership!</h3>
+                    <p className="text-white font-medium w-full md:w-2/3 mx-auto">Join our exclusive membership program and take your experience to the next level! For just <span className="text-3xl font-bold text-red-600">$100</span>, you gain access to a plethora of premium features, ensuring a richer and more rewarding journey on our platform. Enjoy benefits such as early access to new surveys, priority participation in special events, and enhanced data insights. Your support helps us continue delivering top-notch surveys and services. </p>
+                    <div>
+                    </div>
+                </div>
+            </div>
+            <div className="pb-7">
+            <Elements stripe={stripePromise}>
+                {/* form is comming */}
+                <PaymentForm></PaymentForm>
+            </Elements>
+            </div>
         </div>
     );
 };
